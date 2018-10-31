3eba9690e88552db4fc71f95681fe066125390b2
Jia Hao Goh <jiahaog@users.noreply.github.com>
2018-10-30 21:31:48 -0700

    Refactor and implement lexer for digits

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -3,28 +3,39 @@ use std::iter::Peekable;
 use std::str::Chars;
 use token::*;
 
+#[cfg(test)]
+mod tests;
+
 struct Lexer<'a> {
     input: &'a str,
     iter: Peekable<Chars<'a>>,
 }
 
 impl<'a> Lexer<'a> {
-    fn new(input: &'a str) -> Self {
+    pub fn new(input: &'a str) -> Self {
         Lexer {
             input: input,
             iter: input.chars().peekable(),
         }
     }
 
-    fn next_token(&mut self) -> Token<'a> {
-        while let Some(&ch) = self.iter.peek() {
-            if ch != ' ' && ch != '\t' && ch != '\n' {
-                break;
-            }
-
-            self.iter.next();
+    pub fn next_token(&mut self) -> Token<'a> {
+        match self.iter.peek() {
+            Some(&ch) => match ch {
+                ch if is_whitespace(ch) => {
+                    self.iter.next();
+                    return self.next_token();
+                }
+                ch if is_symbol(ch) => self.next_symbol(),
+                ch if ch.is_alphabetic() => self.next_identifier(),
+                ch if ch.is_numeric() => self.next_int(),
+                _ => Token::new(ILLEGAL, String::from("")),
+            },
+            None => Token::new(EOF, String::from("")),
         }
+    }
 
+    fn next_symbol(&mut self) -> Token<'a> {
         match self.iter.next() {
             Some('=') => Token::new(ASSIGN, String::from("=")),
             Some('+') => Token::new(PLUS, String::from("+")),
@@ -34,32 +45,46 @@ impl<'a> Lexer<'a> {
             Some('}') => Token::new(RBRACE, String::from("}")),
             Some(',') => Token::new(COMMA, String::from(",")),
             Some(';') => Token::new(SEMICOLON, String::from(";")),
-            None => Token::new(EOF, String::from("")),
-            Some(token) => if token.is_alphabetic() {
-                let mut literal = String::new();
-
-                literal.push(token);
-
-                while let Some(&ch) = self.iter.peek() {
-                    if !ch.is_alphabetic() {
-                        break;
-                    }
-
-                    literal.push(ch);
-                    self.iter.next();
-                }
-
-                Token::from_literal(literal)
-            } else {
-                Token::new(ILLEGAL, String::from(""))
-            },
+            Some(ch) => Token::new(ILLEGAL, ch.to_string()),
+            None => panic!("None matched for next_symbol"),
         }
     }
 
     fn next_identifier(&mut self) -> Token<'a> {
-        Token::new(EOF, String::from(""))
+        let literal = consume_while(|ch| ch.is_alphabetic(), &mut self.iter);
+        Token::from_literal(literal)
+    }
+
+    fn next_int(&mut self) -> Token<'a> {
+        let literal = consume_while(|ch| ch.is_numeric(), &mut self.iter);
+        Token::new(INT, literal)
     }
 }
 
-#[cfg(test)]
-mod tests;
+fn is_whitespace(ch: char) -> bool {
+    ch == ' ' || ch == '\t' || ch == '\n'
+}
+
+fn is_symbol(ch: char) -> bool {
+    match ch {
+        '=' | '+' | '(' | ')' | '{' | '}' | ',' | ';' => true,
+        _ => false,
+    }
+}
+
+fn consume_while<F>(condition: F, iter: &mut Peekable<Chars>) -> String
+where
+    F: Fn(char) -> bool,
+{
+    let mut literal = String::new();
+
+    while let Some(&ch) = iter.peek() {
+        if !condition(ch) {
+            break;
+        }
+
+        literal.push(ch);
+        iter.next();
+    }
+    literal
+}
