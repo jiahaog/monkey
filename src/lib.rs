35a4b1fe5da68177fccfbcb3e255ca211324626c
Jia Hao Goh <jiahaog@users.noreply.github.com>
2019-01-21 11:09:40 -0800

    Refactor repl to interpreter struct

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,59 @@ mod ast;
 mod eval;
 mod lexer;
 mod parser;
-pub mod repl;
 mod token;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use crate::eval::Env;
+use crate::lexer::Lexer;
+use crate::parser::Parser;
+use std::io::{BufRead, Write};
+
+const PROMPT: &str = ">>> ";
+
+pub struct Interpreter<R: BufRead, W: Write> {
+    env: Env,
+    reader: R,
+    writer: W,
+}
+
+impl<R: BufRead, W: Write> Interpreter<R, W> {
+    pub fn new(reader: R, writer: W) -> Self {
+        Self {
+            env: Env::new(),
+            reader: reader,
+            writer: writer,
+        }
+    }
+
+    pub fn start(&mut self) {
+        loop {
+            self.writer.write(PROMPT.as_bytes()).unwrap();
+            self.writer.flush().unwrap();
+
+            let mut buf = String::new();
+            self.reader.read_line(&mut buf).unwrap();
+
+            let output = handle_input(self.env.clone(), &mut self.writer, buf);
+            output.expect("no errors for as_bytes()");
+        }
+    }
+}
+
+fn handle_input<W>(
+    env: Env,
+    output: &mut W,
+    s: String,
+) -> std::result::Result<usize, std::io::Error>
+where
+    W: Write,
+{
+    // TODO cleanup output
+    match Parser::new(Lexer::new(&s)).parse() {
+        Ok(program) => {
+            let object = program.evaluate(env);
+
+            output.write(format!("{:?}\n", object).as_bytes())
+        }
+        Err(e) => output.write(format!("{:?}\n", e).as_bytes()),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 extern crate monkey;
 
-use monkey::repl;
 use std::io;
 
 fn main() {
     let stdin = io::stdin();
     let stdout = io::stdout();
-    repl::start(stdin.lock(), stdout.lock());
+
+    monkey::Interpreter::new(stdin.lock(), stdout.lock()).start();
 }
